ee3102b61ce024e56e9ca9c7e5276a4ae71a4c3e
Joel Reyes <jreyesguerrero@deloitte.com>
2024-09-09 11:59:04 -0600

    Added database service
    Modified DatabaseContext class to solve a connection error with the database tables.
    Created DatabaseService class to manage query information.

--- Practice4/Repository/DatabaseContext.cs
+++ Practice4/Repository/DatabaseContext.cs
@@ -5,13 +5,17 @@ namespace Practice4.Repository
 {
     public class DatabaseContext : DbContext
     {
-        public DbSet<Product> Products { get; set; }
-        public DbSet<Sale> Sales { get; set; }
-        public DbSet<Purchase> Purchases { get; set; }
+        public DbSet<Product> Product { get; set; }
+        public DbSet<Sale> Sale { get; set; }
+        public DbSet<Purchase> Purchase { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer("Server=USQROJREYESGUE1;Database=Practice4;Trusted_Connection=True;TrustServerCertificate=True;");
+            optionsBuilder.UseSqlServer(
+                "Server=USQROJREYESGUE1;" +
+                "Database=Practice4;" +
+                "Trusted_Connection=True;" +
+                "TrustServerCertificate=True;");
         }
     }
 }
---
+++ Practice4/Service/DatabaseService.cs
@@ -0,0 +1,101 @@
+﻿using Practice4.Entity;
+using Practice4.Entity.DTO;
+using Practice4.Repository;
+
+namespace Practice4.Service
+{
+    public class DatabaseService
+    {
+        private readonly int currentMonth;
+        private readonly int currentYear;
+        private readonly DatabaseContext context;
+
+        public DatabaseService(DatabaseContext context)
+        {
+            currentMonth = DateTime.Now.Month;
+            currentYear = DateTime.Now.Year;
+            this.context = context;
+        }
+
+        public List<Product> GetAllProducts()
+        {
+            return context.Product.ToList();
+        }
+
+        public bool CreateProduct(string name, decimal price, string category)
+        {
+            context.Add(new Product
+            {
+                Name = name,
+                Price = price,
+                Category = category,
+                DateAdded = DateTime.Now
+            });
+
+            return context.SaveChanges() > 0;
+        }
+
+        public List<Product> GetProductByIdOrName(string query)
+        {
+            return context.Product
+                .Where(product => product.Name == query
+                        || product.ProductId.ToString() == query)
+                .ToList();
+        }
+
+        public List<DailySale> GetDailySales()
+        {
+            return context.Sale
+                .GroupBy(sale => sale.SaleDate)
+                .Select(group => new DailySale(group.Key,
+                        group.Sum(sale => sale.SalePrice * sale.Quantity)))
+                .OrderByDescending(group => group.TotalSale)
+                .ToList();
+        }
+
+        public List<SaleDetails> GetSaleDetails()
+        {
+            return context.Sale
+                .Where(sale => sale.SaleDate.Month == currentMonth
+                        && sale.SaleDate.Year == currentYear)
+                .Join(context.Product,
+                    sale => sale.ProductID,
+                    product => product.ProductId,
+                    (sale, product) => new SaleDetails(sale.SaleID, product.Name,
+                            sale.Quantity, sale.SalePrice, sale.SaleDate))
+                .ToList();
+        }
+
+        public List<Product> GetProductsWithNoSales()
+        {
+            return context.Product
+                .GroupJoin(context.Sale,
+                    product => product.ProductId,
+                    sale => sale.ProductID,
+                    (product, sale) => new { product, sale })
+                .SelectMany(ps => ps.sale.DefaultIfEmpty(),
+                    (ps, sale) => new { ps.product, sale })
+                .Where(ps => ps.sale == null)
+                .Select(ps => ps.product)
+                .ToList();
+        }
+
+        public List<ProductSale> GetProductSales()
+        {
+            return context.Sale
+                .Where(sale => sale.SaleDate.Month == currentMonth
+                        && sale.SaleDate.Year == currentYear)
+                .GroupBy(sale => sale.ProductID)
+                .Select(group => new
+                {
+                    ProductId = group.Key,
+                    TotalSale = group.Sum(sale => sale.SalePrice * sale.Quantity),
+                })
+                .Join(context.Product,
+                    group => group.ProductId,
+                    product => product.ProductId,
+                    (group, product) => new ProductSale(product.Name, group.TotalSale))
+                .ToList();
+        }
+    }
+}
